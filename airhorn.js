271e78edc1348aedd409c43f0bf81ede6f63c713
Moto42 <40679192+Moto42@users.noreply.github.com>
2019-05-12 20:12:35 -0500

    Set up the UserScript in the greasemonkey script.

---
+++ airhorn.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         Airhorn for AirOS
+// @namespace    m42_airhorn
+// @version      0.1
+// @description  Provides audible cues to help establish connection with another radio, as specified by it's MAC address.
+// @author       Wesley Williams moto42@gmail.com
+// @match        https://www.tampermonkey.net/index.php?version=4.8.41&ext=dhdg&updated=true
+// @grant        none
+// @run-at       document-end
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Your code here...
+})();
