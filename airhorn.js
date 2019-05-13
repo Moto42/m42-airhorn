1a3e7ff423f55ce9567efe63976aae39dafde038
Moto42 <40679192+Moto42@users.noreply.github.com>
2019-05-12 21:40:38 -0500

    Airhorn Button added Button now clears the 'body' section of the page, clearing the way for our content to be added.

--- airhorn.js
+++ airhorn.js
@@ -2,15 +2,44 @@
 // @name         Airhorn for AirOS
 // @namespace    m42_airhorn
 // @version      0.1
+// @include      *
 // @description  Provides audible cues to help establish connection with another radio, as specified by it's MAC address.
 // @author       Wesley Williams moto42@gmail.com
-// @match        https://www.tampermonkey.net/index.php?version=4.8.41&ext=dhdg&updated=true
 // @grant        none
 // @run-at       document-end
 // ==/UserScript==
 
+function getBodyCell(){
+  return document.querySelector('body > table > tbody > tr:nth-child(3) > td');
+}
+
+function handleAirhornButton(){
+  clearContent();
+}
+
+// add my own menu button to the bar at the top of the screen.
+function injectMenuButton() {
+  const menuBar = document.querySelector('body > table > tbody > tr.menu > td:nth-child(1)');
+  const btn     = document.createElement("BUTTON");
+  btn.innerHTML = "AirHorn";
+  btn.setAttribute('id','airhornButton');
+  btn.addEventListener('click',handleAirhornButton);
+  btn.style.position = 'relative';
+  btn.style.bottom = '7px';
+  btn.style.borderRadius = '.5rem .5rem 0px 0px';
+  btn.style.borderStyle = 'none';
+  menuBar.appendChild(btn);
+}
+
+//Clear all the old content out of the main div to make way for my content
+function clearContent() {
+  const bodyCell = getBodyCell();
+  bodyCell.innerHTML = '';
+}
+
 (function() {
     'use strict';
 
-    // Your code here...
+    injectMenuButton();
+
 })();
